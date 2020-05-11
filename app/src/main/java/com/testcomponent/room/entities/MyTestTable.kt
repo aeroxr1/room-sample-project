21d1478947497e18731bac377576bf2512fa8904
grugani <gabriele.rugani@apparound.com>
2020-05-11 22:40:06 +0200

    Edit the sample with another crash case.

--- app/src/main/java/com/testcomponent/room/entities/MyTestTable.kt
+++ app/src/main/java/com/testcomponent/room/entities/MyTestTable.kt
@@ -4,7 +4,8 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 
 @Entity(tableName = "MyTestTable", primaryKeys= [ "Id", "Params" ])
-data class MyTestTable(@ColumnInfo(name = "Id") var Id: Int,
+data class MyTestTable (  @ColumnInfo(name = "Id") var Id: Int,
                           @ColumnInfo(name = "Params") var Params: String,
-                          @ColumnInfo(name = "Rank") var Rank: Int
+                          @ColumnInfo(name = "Rank") var Rank: Int?,
+                          @ColumnInfo(name="Date") var Date: String?
 )
